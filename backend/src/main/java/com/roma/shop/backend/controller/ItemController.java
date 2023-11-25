7ded1be6b566d2ea373a6d490b69d6f82be4cf8a
kjspo56 <kjspo56@gmail.com>
2023-11-26 02:00:52 +0900

    repo 변경 테스트

---
+++ backend/src/main/java/com/roma/shop/backend/controller/ItemController.java
@@ -0,0 +1,25 @@
+package com.roma.shop.backend.controller;
+
+import com.roma.shop.backend.entity.Item;
+import com.roma.shop.backend.repository.ItemRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class ItemController {
+
+    private final ItemRepository itemRepository;
+
+    public ItemController(ItemRepository itemRepository) {
+        this.itemRepository = itemRepository;
+    }
+
+    //Todo: service로 분리
+    @GetMapping("/api/items")
+    public List<Item> getItems(){
+        List<Item> items = itemRepository.findAll();
+        return items;
+    }
+}
