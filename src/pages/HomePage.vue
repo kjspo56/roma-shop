8ae41222a2b76f0109de1e3997f0267890793448
kjspo56 <kjspo56@gmail.com>
2023-11-26 01:14:58 +0900

    [F] axios get test

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -15,8 +15,9 @@
     <div class="album py-5 bg-body-tertiary">
       <div class="container">
         <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
-          <div class="col" v-for="i in 12" :key="i">
-          <CardBox/>
+          <div class="col" v-for="(item, idx) in state.items" :key="idx">
+            {{ items }}
+          <CardBox :item="item"/>
           </div>
         </div>
       </div>
@@ -25,10 +26,23 @@
 </template>
 <script>
 import CardBox from "@/components/CardBox.vue";
+import axios from "axios";
+import {reactive} from "vue";
 
 export default {
   name: "HomePage",
-  components: {CardBox}
+  components: {CardBox},
+  setup() { //setUp이라고 하면 안되는구나
+    const state = reactive({
+      items:[]
+    })
+
+    axios.get("/api/items").then(({data}) => {
+      state.items = data;
+    });
+
+    return {state}
+  }
 }
 </script>
 <style scoped>
