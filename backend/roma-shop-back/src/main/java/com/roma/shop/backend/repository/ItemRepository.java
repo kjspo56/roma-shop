d3109acad242606b811037df65080f008b62b977
kjspo56 <kjspo56@gmail.com>
2023-11-26 01:56:08 +0900

    repository 변경 테스트

--- backend/roma-shop-back/src/main/java/com/roma/shop/backend/repository/ItemRepository.java
+++
@@ -1,8 +0,0 @@
-package com.roma.shop.backend.repository;
-
-import com.roma.shop.backend.entity.Item;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ItemRepository extends JpaRepository<Item, Integer> {
-
-}
